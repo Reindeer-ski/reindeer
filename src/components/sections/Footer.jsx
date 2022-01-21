7e388942e795227c76022e2eeb2ae97e6740cced
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-01-21 18:04:27 +0530

    Footer And Navbar

---
+++ src/components/sections/Footer.jsx
@@ -0,0 +1,43 @@
+import { Flex, Link, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+
+const Footer = (props) => {
+	const bgColor = useColorModeValue('white.50', 'gray.900');
+
+	return (
+		<Flex
+			as="nav"
+			align="center"
+			justify="space-between"
+			w="100%"
+			p={2}
+			px={4}
+			shadow="sm"
+			bgColor={bgColor}
+			pos={'sticky'}
+			top={0}>
+			<NavLink to="/">Reindeer</NavLink>
+			Footer
+			<VStack>
+				<NavLink to="/">Home</NavLink>
+				<NavLink to="/about">About</NavLink>
+			</VStack>
+		</Flex>
+	);
+};
+
+const NavLink = ({ children, to }) => (
+	<Link
+		as={RouterLink}
+		p={2.5}
+		to={to}
+		rounded={'md'}
+		_hover={{
+			textDecoration: 'none',
+			bg: useColorModeValue('blackAlpha.50', 'whiteAlpha.200'),
+		}}>
+		{children}
+	</Link>
+);
+
+export default Footer;
