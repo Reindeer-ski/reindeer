abeaab68f148e2acd6e9a5a1857782081b4ef090
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-01-21 18:04:54 +0530

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import { Suspense } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Routes, Route, Navigate } from 'react-router';
-import Navbar from './components/Navbar';
 import Landing from './pages/Landing';
 import extendedTheme from './theme';
+import DefaultPage from './components/layouts/DefaultPage';
 
 const App = () => {
 	return (
 		<ChakraProvider theme={extendedTheme}>
-			<Navbar />
-			<Routes>
-				<Route path="/">
-					<Route
-						exact
-						path=""
-						element={
-							<Suspense fallback={<>Loading</>}>
-								<Landing />
-							</Suspense>
-						}
-					/>
-					<Route
-						exact
-						path="/about"
-						element={<Suspense fallback={<>Loading</>}>About</Suspense>}
-					/>
-					<Route exact path="404" element={<>404 Page not found</>} />
-					<Route path="*" element={<Navigate to="/404" />} />
-				</Route>
-			</Routes>
+			<DefaultPage>
+				<Routes>
+					<Route path="/">
+						<Route
+							exact
+							path=""
+							element={
+								<Suspense fallback={<>Loading</>}>
+									<Landing />
+								</Suspense>
+							}
+						/>
+						<Route
+							exact
+							path="/about"
+							element={<Suspense fallback={<>Loading</>}>About</Suspense>}
+						/>
+						<Route exact path="404" element={<>404 Page not found</>} />
+						<Route path="*" element={<Navigate to="/404" />} />
+					</Route>
+				</Routes>
+			</DefaultPage>
 		</ChakraProvider>
 	);
 };
