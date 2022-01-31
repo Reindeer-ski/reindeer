41a95ce16c9469ba7a683d415f32dbfcf0c84fb2
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-01-31 21:12:44 +0530

    Subscribe to dapps

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -6,10 +6,13 @@ import {
 	InputLeftElement,
 	Input,
 	Button,
+	useToast,
 } from '@chakra-ui/react';
 import { GoSearch as Search } from 'react-icons/go';
 import Page from '../components/layouts/Page';
 import DAppCardLayout from '../components/DAppCardLayout';
+import { useMoralis, useMoralisCloudFunction } from 'react-moralis';
+import { useRef } from 'react';
 
 const Explore = () => {
 	return (
@@ -35,41 +38,108 @@ const Explore = () => {
 						/>
 					</InputGroup>
 				</Flex>
-				<Flex
-					direction='row'
-					w={'100%'}
-					gap={5}
-					align='center'
-					justify='center'
-					wrap='wrap'
-					mt='5'>
-					<DAppCardLayout
-						name={'Snowball'}
-						// key='0x'
-						imageURL={
-							'https://pbs.twimg.com/profile_images/1471064848876425224/N40TS_20_400x400.png'
-						}>
-						<Button>Subscribe</Button>
-					</DAppCardLayout>
-					<DAppCardLayout
-						name={'Uniswap'}
-						// key='0x'
-						imageURL={
-							'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Uniswap_Logo.svg/1026px-Uniswap_Logo.svg.png'
-						}>
-						<Button>Subscribe</Button>
-					</DAppCardLayout>
-					<DAppCardLayout
-						name={'dHEDGE'}
-						// key='0x'
-						imageURL={
-							'https://www.newsbtc.com/wp-content/uploads/2020/09/dhedge-img.png'
-						}>
-						<Button>Subscribe</Button>
-					</DAppCardLayout>
-				</Flex>
+				<DAppCards />
 			</Box>
 		</Page>
+	);
+};
+const DAppCards = () => {
+	const { user, web3, isWeb3Enabled } = useMoralis();
+	const { fetch: subscribeToDApp } = useMoralisCloudFunction(
+		'subscribe',
+		{},
+		{ autoFetch: false }
+	);
+	const toast = useToast();
+	const toastIdRef = useRef();
+
+	const handleSubscribe = async (dAppAddress, topic = 'all', name) => {
+		toastIdRef.current = toast({
+			title: `Subscribing to ${name}`,
+			description: 'Sign the message to confirm',
+			status: 'info',
+			duration: null,
+			isClosable: true,
+		});
+		if (isWeb3Enabled) {
+			try {
+				const signer = web3.getSigner(user.get('ethAddress'));
+				const message = `sender=${dAppAddress.toLowerCase()}&topic=${topic}&receiver=${user
+					.get('ethAddress')
+					.toLowerCase()}&platform=1111`;
+
+				const signature = await signer.signMessage(message);
+				console.log(message, signature);
+
+				subscribeToDApp({
+					params: {
+						sender: dAppAddress,
+						receiver: user.get('ethAddress'),
+						topic,
+						platform: '1111',
+						signature,
+					},
+					onSuccess: (response) => {
+						toast({
+							title: 'Success',
+							description: 'You have successfully subscribed to this Address',
+							status: 'success',
+							duration: 9000,
+							isClosable: true,
+						});
+					},
+					onError: (error) => {
+						toast({
+							title: 'Error',
+							description: error.message,
+							status: 'error',
+							duration: 9000,
+							isClosable: true,
+						});
+					},
+				});
+			} catch (err) {
+				console.log(err);
+				toast({
+					title: 'Error',
+					description: err.message,
+					status: 'error',
+					duration: 9000,
+					isClosable: true,
+				});
+			}
+			toast.close(toastIdRef.current);
+		}
+	};
+	return (
+		<Flex
+			direction='row'
+			w={'100%'}
+			gap={5}
+			align='center'
+			justify='center'
+			wrap='wrap'
+			mt='5'>
+			<DAppCard
+				name='Your Address'
+				address={user.get('ethAddress').toLowerCase()}
+				handleSubscribe={handleSubscribe}
+				imageURL={'https://avatars.githubusercontent.com/u/11744586?s=280&v=4'}
+			/>
+		</Flex>
+	);
+};
+
+const DAppCard = ({ name, imageURL, address, dAppLink, handleSubscribe }) => {
+	return (
+		<DAppCardLayout name={name} imageURL={imageURL} dAppLink={dAppLink}>
+			<Button
+				onClick={() => {
+					handleSubscribe(address);
+				}}>
+				Subscribe
+			</Button>
+		</DAppCardLayout>
 	);
 };
 
